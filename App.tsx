648060bc88f31cf0fd78fe4300df406953d6fa0a
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-04-24 20:27:21 +0200

    Setup Sass & Fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { fonts } from './utils/fonts';
 
 export default function App() {
+
+
+	// load the fonts
+
+	const [fontsLoaded] = useFonts(fonts)
+
+	// don't display anything until the fonts are loaded
+
+	if(!fontsLoaded) return <View></View>
+
+	// render
+
 	return (
 		<View>
 			<Text>Open up App.tsx to start working on your app!</Text>
